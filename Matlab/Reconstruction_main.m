173ac4b63464b4b9cefbafa6db6e6e27546444d1
Ric Shaw <richard.o.shaw@gmail.com>
2015-03-02 08:24:06 +0000

    added dynamic 3D plot of face

--- Matlab/Reconstruction_main.m
+++ Matlab/Reconstruction_main.m
@@ -1,5 +1,7 @@
 %% Reconstruction_main.m
-clear; close all;
+close all; clc;
+clearvars -except Richard2_left_vx500 Richard2_left_vy500 ...
+                    Richard2_right_vx500 Richard2_right_vy500;
 
 %% Read a pair of rectified stereo images
 folder_left = 'C:\Users\Richard\Desktop\CDE\Semester 2\Visual Effects\Data\Richard2\Left\images_rect\';
@@ -27,7 +29,9 @@ P_left = K_left * eye(3,4);
 P_right = K_right * [R, t];
 
 % Clear variables
-clearvars -except P_left P_right K_left K_right R t im_left im_right
+clearvars -except P_left P_right K_left K_right R t im_left im_right ...
+                    Richard2_left_vx500 Richard2_left_vy500 ...
+                    Richard2_right_vx500 Richard2_right_vy500;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -146,7 +150,7 @@ im_left = im2double(im_left);
 im_right = im2double(im_right);
 fStore_right = fStore;
 
-w = 4; % Image patch size = (2*w + 1) x (2*w + 1) pixels
+w = 5; % Image patch size = (2*w + 1) x (2*w + 1) pixels
 NC = NaN(1,640);
 for i = 1:numFeatures % For each feature point in left image...
     col = round(fStore(1,i)); % x-coordinate
@@ -156,7 +160,7 @@ for i = 1:numFeatures % For each feature point in left image...
     std_left = std(window_left(:)); % Std dev. of image patch
     window_left_shift = window_left - mean_left; % Patch shifted by mean
         
-    for j = 250+w:500-w % For points in right image on same row...
+    for j = col-20:col+60 % For points in right image on same row...
         window_right = im_right(row-w:row+w, j-w:j+w); % Right image patch
         mean_right = mean(window_right(:)); % Mean of image patch
         std_right = std(window_right(:)); % Std dev. of image patch
@@ -167,8 +171,8 @@ for i = 1:numFeatures % For each feature point in left image...
     end    
     
     [val,ind] = max(NC); % Max cross-correlation
-    % While distance between feature points is greater than 50px...
-    while(abs(ind-col) > 50) 
+    % While distance between feature points is greater than 60px...
+    while(abs(ind-col) > 60) 
         NC(ind) = NaN;
         [val,ind] = max(NC); % Choose next best...
     end   
@@ -188,7 +192,7 @@ for i = 1:numFeatures
 end
 
 %% 3D reconstruction
-X = Reconstruct(P_left, P_right, fStore, fStore_right);
+X = Reconstruct(P_left, P_right, fStore(:,:,1), fStore_right(:,:,1));
 
 %% Visualise
 VisualiseScene(P_left, P_right, X);
@@ -204,4 +208,129 @@ trisurf(tri, x, y, z, 'LineWidth', 2);
 axis equal
 shading interp
 colormap(cool)
-camlight right
+% camlight right
+light % create a light
+lighting gouraud
+material dull
+% alpha(0.8) 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Read in left image sequence
+if isunix
+    folder = '~/workspaces/matlab/vfx/Data/Richard2/Left/images_rect/';
+else
+    folder = 'C:\Users\Richard\Desktop\CDE\Semester 2\Visual Effects\Data\Richard2\Left\images_rect';
+end
+imgType = '.jpg';
+numImgs = 500;
+imArray_left = ReadImgs(folder, imgType, numImgs);
+
+%% Read in right image sequence
+if isunix
+    folder = '~/workspaces/matlab/vfx/Data/Richard2/Left/images_rect/';
+else
+    folder = 'C:\Users\Richard\Desktop\CDE\Semester 2\Visual Effects\Data\Richard2\Right\images_rect';
+end
+imgType = '.jpg';
+numImgs = 500;
+imArray_right = ReadImgs(folder, imgType, numImgs);
+
+%% Load optical flow
+folder = 'C:\Users\Richard\Desktop\CDE\Semester 2\Visual Effects\Data\Richard2\';
+disp('Loading vx and vy');
+load([folder 'Richard2_left_vx500']);
+load([folder 'Richard2_left_vy500']);
+load([folder 'Richard2_right_vx500']);
+load([folder 'Richard2_right_vy500']);
+disp('Done!');
+    
+%% Track features in left image
+disp('Tracking features');
+newImg = imArray_left{1};
+for j = 1:numFeatures
+    newImg(round(fStore(2,j,1)) - 2:2 + round(fStore(2,j,1)), ...
+        round(fStore(1,j,1)) - 2:round(fStore(1,j,1)) + 2 ) = 255;
+end
+
+Mov(1) = im2frame(flipud(newImg), gray(256));
+for frame = 2:numImgs
+    newImg = imArray_left{frame};
+    for j = 1:numFeatures
+        idx = round(fStore(1,j,frame-1));
+        idy = round(fStore(2,j,frame-1));
+        fStore(1,j,frame) = fStore(1,j,frame-1) + Richard2_left_vx500(idy,idx,frame-1);
+        fStore(2,j,frame) = fStore(2,j,frame-1) + Richard2_left_vy500(idy,idx,frame-1);
+        
+        newImg(round(fStore(2,j,frame)) - 2:2 + round(fStore(2,j,frame)), ...
+            round(fStore(1,j,frame)) - 2:round(fStore(1,j,frame)) + 2 ) = 255;
+    end
+    Mov(frame) = im2frame(flipud(newImg), gray(256));
+end
+
+%% Play movie
+figure; imshow(imArray_left{1});
+movie(Mov,1,60);
+
+%% Track features in right image
+disp('Tracking features');
+newImg = imArray_right{1};
+for j = 1:numFeatures
+    newImg(round(fStore_right(2,j,1)) - 2:2 + round(fStore_right(2,j,1)), ...
+        round(fStore_right(1,j,1)) - 2:round(fStore_right(1,j,1)) + 2 ) = 255;
+end
+
+Mov(1) = im2frame(flipud(newImg), gray(256));
+for frame = 2:numImgs
+    newImg = imArray_right{frame};
+    for j = 1:numFeatures
+        idx = round(fStore_right(1,j,frame-1));
+        idy = round(fStore_right(2,j,frame-1));
+        fStore_right(1,j,frame) = fStore_right(1,j,frame-1) + Richard2_right_vx500(idy,idx,frame-1);
+        fStore_right(2,j,frame) = fStore_right(2,j,frame-1) + Richard2_right_vy500(idy,idx,frame-1);
+        
+        newImg(round(fStore_right(2,j,frame)) - 2:2 + round(fStore_right(2,j,frame)), ...
+            round(fStore_right(1,j,frame)) - 2:round(fStore_right(1,j,frame)) + 2 ) = 255;
+    end
+    Mov(frame) = im2frame(flipud(newImg), gray(256));
+end
+
+%% Play movie
+figure; imshow(imArray_right{1});
+movie(Mov,1,60);
+
+%% Apply epipolar constraint
+fStore(2,:) = (fStore(2,:) + fStore_right(2,:))./2; % Set y-coordinate to average
+fStore_right(2,:) = fStore(2,:);
+
+%% Reconstruct all frames
+X = NaN(4,numFeatures,numImgs);
+for frame = 1:numImgs
+    X(:,:,frame) = Reconstruct(P_left, P_right, fStore(:,:,frame), fStore_right(:,:,frame));
+end
+
+%% Show 4D plot
+for frame = 1:500
+    x_offset = X(1,54,frame); % Keep nose position at x = 0
+    y_offset = X(3,54,frame); % Keep nose position at y = 0
+    z_offset = -X(2,54,frame); % Keep nose position at z = 0
+    
+    x = X(1,:,frame)-x_offset;
+    y = X(3,:,frame)-y_offset;
+    z = -X(2,:,frame)-z_offset;
+    
+    figure(6);
+    trisurf(tri, x, y, z, 'LineWidth', 1.5);
+    axis equal
+    axis([-50 50 0 80 -60 80])
+    colormap(cool)
+    camlight right
+    light % create a light
+    lighting gouraud
+    material dull
+    view([90,-180,45]);
+end
+
+    
