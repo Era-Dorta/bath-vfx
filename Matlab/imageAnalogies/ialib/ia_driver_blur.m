14c92620c9ddb482b7b3bc497842be663ff0687b
Garoe <garoedp@gmail.com>
2015-03-23 17:15:31 +0000

    Added comments and simplified code in ia library

--- Matlab/imageAnalogies/ialib/ia_driver_blur.m
+++ Matlab/imageAnalogies/ialib/ia_driver_blur.m
@@ -7,32 +7,34 @@
 % This was done to avoid the cost of processing each color channel
 % separately (which is what should probably be done).
 %
-clear all
+clear all;
+close all;
 
 A0 = imread('~/workspaces/matlab/vfx/Data/skinRender/imageAnalogies/A0.jpg');
 A1 = imread('~/workspaces/matlab/vfx/Data/skinRender/imageAnalogies/A1.jpg');
 B0 = imread('~/workspaces/matlab/vfx/Data/skinRender/imageAnalogies/B0.jpg');
 
-A0 = imresize(A0, 0.2);
-A1 = imresize(A1, 0.2);
-B0 = imresize(B0, 0.2);
+A0 = imresize(A0, 0.25);
+A1 = imresize(A1, 0.25);
+B0 = imresize(B0, 0.25);
 
-imA = rgb2hsv(im2double(A0));
-imAp = rgb2hsv(im2double(A1));
-imB = rgb2hsv(im2double(B0));
+imA = rgb2gray(im2double(A0));
+imAp = rgb2gray(im2double(A1));
+imB = rgb2gray(im2double(B0));
 
 % Decrease to increase the image distortion
-opt.sourceWeight = 0.3;
+opt.sourceWeight = 0.5;
+options.scaleFeatures = false;
 
-imBp = ia_process(imA(:,:,3), imAp(:,:,3), imB(:,:,3), 'testim', opt);
+imBp = ia_process(imA(:,:), imAp(:,:), imB(:,:), 'testim', opt);
 
 % add the H and S channels to the 
-imBp = cat(3, imB(:,:,[1 2]), imBp);
-
-imA = hsv2rgb(imA);
-imAp = hsv2rgb(imAp);
-imB = hsv2rgb(imB);
-imBp = hsv2rgb(imBp);
+% imBp = cat(3, imB(:,:,[1 2]), imBp);
+% 
+% imA = hsv2rgb(imA);
+% imAp = hsv2rgb(imAp);
+% imB = hsv2rgb(imB);
+% imBp = hsv2rgb(imBp);
 
 figure(1); clf;
 subplot(221); imshow(imA); title('A')
--- Matlab/imageAnalogies/ialib/ia_process.m
+++ Matlab/imageAnalogies/ialib/ia_process.m
@@ -24,6 +24,11 @@ h = fspecial('gaussian', [5 5], 2);
 
 % since we are going in column order, this is a mask of where already
 % filled in pixels will be in our output image
+%      1     1     1     1     1
+%      1     1     1     1     1
+%      1     1     0     0     0
+%      0     0     0     0     0
+%      0     0     0     0     0
 goodPixels = true(5,5); goodPixels(3,3:5) = false; goodPixels(4:5,:) = false;
 
 szA = size(imA);
@@ -36,39 +41,40 @@ fB = imB;
 
 % scale the features to be in the correct range
 if options.scaleFeatures
-    fA = fA - mean(fA(:)); fA = fA ./ norm(fA(:));
-    fB = fB - mean(fB(:)); fB = fB ./ norm(fB(:));
+    fA = fA - mean(fA(:)); 
+    fA = fA ./ norm(fA(:));
+    
+    fB = fB - mean(fB(:)); 
+    fB = fB ./ norm(fB(:));
 end
 
 %
 % build features from A (using a 5x5 neighborhood)
-% stack different channels vertically
-%
-fAneigh = [];
-for ix = 1:size(fA,3)
-    fAneigh = ...
-        [fAneigh
-        im2col(padarray(fA(:,:,ix), [2 2], 'replicate'), [5 5], 'sliding')];
-end
+% 
+% Pad array adds 2 rows and colums at the beggining and the end of the 
+% image, copying the values of the first/last column/row.
+% im2col rearrangles blocks into colums, so 5x5 blocks around each
+% pixel are a colum in fAneigh
+% x - 2, y - 2 ... x + 2, y - 2
+%   .                   .
+%   .                   .
+% x - 2, y + 2 ... x + 2, y + 2
+% The first column in the block are the first n elements in the feature
+% column, then then next column, until the last
+fAneigh = im2col(padarray(fA, [2 2], 'replicate'), [5 5], 'sliding');
 
 % apply gaussian weights to empahsize the middle pixel
-fAneigh = bsxfun(@times, repmat(h(:), [size(fA,3), 1]), fAneigh);
+fAneigh = bsxfun(@times, h(:), fAneigh);
 
 %
 % build features for A'
-%
-fApneigh = [];
-for ix = 1:size(imAp,3)
-    fApneigh = ...
-        [fApneigh
-        im2col(padarray(imAp(:,:,ix), [2 2], 0), [5 5], 'sliding')];
-end
+% padarray, adding the two columns and rows, but of zeros
+fApneigh = im2col(padarray(imAp, [2 2], 0), [5 5], 'sliding');
 
 % apply gaussian weights to empahsize the middle pixel (also ignore empty
 % pixels in B')
-fApneigh = fApneigh(repmat(goodPixels, [size(imAp, 3), 1]),:);
-fApneigh = bsxfun(@times, ...
-    repmat(h(goodPixels), [size(imAp, 3), 1]), fApneigh);
+fApneigh = fApneigh(goodPixels,:);
+fApneigh = bsxfun(@times, h(goodPixels), fApneigh);
 
 % weight source and target image equally
 scaA = mean(sqrt(sum(fAneigh.^2,1))) \ (options.sourceWeight);
@@ -82,7 +88,14 @@ fApneigh = fApneigh .* scaAp;
 
 dataset = [fAneigh;fApneigh];
 
+% flann_par.algorithm = 'autotuned';
+% flann_par.target_precision = 0.9;
+% flann_par.build_weight = 0.01;
+% flann_par.memory_weight = 0;
+% flann_par.sample_fraction = 0.1;
+
 [index, params] = flann_build_index(dataset, struct);
+%params.cores = 4;
 
 imBp = nan(szB);
 
@@ -104,11 +117,11 @@ for ixB = 1:szB(1)
         feat = fBpad(ixB + (0:4), jxB + (0:4),:);
         fBppad = padarray(imBp, [2 2], 0); % lazy padding to avoid edge cases
         featp = fBppad(ixB + (0:4), jxB + (0:4),:);
-        featp = featp(repmat(goodPixels, [1 1 size(imAp,3)]));
+        featp = featp(goodPixels);
         
         % apply gaussian weights to empahsize the middle pixel
         feat = bsxfun(@times, feat, h);
-        featp = bsxfun(@times, featp, repmat(h(goodPixels), [size(imAp,3) 1]));
+        featp = bsxfun(@times, featp, h(goodPixels));
         feat = [feat(:).*scaA; featp.*scaAp];
         
         %
