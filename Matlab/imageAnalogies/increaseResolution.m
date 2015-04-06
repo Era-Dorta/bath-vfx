118458ce5adb6f74a3e0295052c9945bec355b3d
Garoe <garoedp@gmail.com>
2015-04-06 14:21:31 +0100

    Find square in texture that contains the current segment

--- Matlab/imageAnalogies/increaseResolution.m
+++ Matlab/imageAnalogies/increaseResolution.m
@@ -1,6 +1,8 @@
+%% Clean up environment
 clear all;
 close all;
 
+%% Loading and initialization
 segments_centres = [2540, 556;
     3260, 2124;
     2212, 3716;
@@ -17,12 +19,33 @@ segments_centres = [segments_centres(:,2), segments_centres(:,1)];
 
 
 face_segmented = imread('~/workspaces/matlab/vfx/Data/skinRender/microgeometry/face_segmented.png');
+face_tex = imread('~/workspaces/matlab/vfx/Data/skinRender/3dscans/vfx_richard3_face_simplified_0.png');
+bump_map = histeq(rgb2gray(face_tex));
+%imshow(bum_map);
 
 % 0 is background, 1 are lines
 face_segmented = ~face_segmented;
 
+%% Texture up-sampling
+
 % Get the first section
 filled_with_bg = imfill(face_segmented,segments_centres(1,:));
-% Delete the extra lines, i.e. substract common pixels in both images
-only_region = filled_with_bg - (filled_with_bg & face_segmented);
-imshow(only_region);
+
+% Delete the extra lines, i.e. subtract common pixels in both images
+only_region = logical(filled_with_bg - (filled_with_bg & face_segmented));
+
+% Find the smallest square that contains the region
+[indx, indy] = find(only_region == 1);
+max_indx = max(indx);
+min_indx = min(indx);
+max_indy = max(indy);
+min_indy = min(indy);
+
+x = [max_indx, min_indx, min_indx, max_indx, max_indx];
+y = [max_indy, max_indy, min_indy, min_indy, max_indy];
+
+bump_map1 = bump_map;
+bump_map1(only_region) = 0;
+imshow(bump_map1);
+hold on;
+plot(y, x, 'b-', 'LineWidth', 3);
