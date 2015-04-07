85b7d63f5de2b1170d05c6375858e1762a49bffa
Garoe <garoedp@gmail.com>
2015-04-07 11:36:41 +0100

    Image analogies script now runs a loop for all the images

--- python/run_image_analogies.sh
+++ python/run_image_analogies.sh
@@ -46,9 +46,17 @@ PY_SCRIPT_PATH=$DIR"/image_analogies_parallel.py"
 ##############################################################
 # Full command of the job
 ##############################################################
+cd ~/workspaces/matlab/vfx/Data/skinRender/microgeometry
+# Run 4 processes for each image
+I=1
+while [  $I -lt 11 ]; do
+	mpiexec -n 4 python $PY_SCRIPT_PATH "./original/A0_"$I".png" "./original/A1_"$I".png" "./synthesized/B0_"$I".png" "./synthesized/B1_"$I".png"
+	let I=I+1 
+done
+
 #mpiexec -n 4 --mca btl_tcp_if_include ib0 python image_analogies_parallel.py artistic1_A1.jpg artistic1_A2.jpg fun.jpg fun_2.jpg
 #mpiexec -n 4 python image_analogies_parallel.py A0g.png A1g.png B0g.png B1g.png
-mpiexec -n 4 python $PY_SCRIPT_PATH $1 $2 $3 $4
+#mpiexec -n 4 python $PY_SCRIPT_PATH $1 $2 $3 $4
 #mpiexec -n 4 python image_analogies_parallel.py embossA1g.png embossA2g.png blurB1.png embossB2g.png
 #
 ##############################################################
