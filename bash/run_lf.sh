8219ef7ab75f96568eac816d2f920a4b31c0fc02
Garoe <garoedp@gmail.com>
2015-04-14 12:26:58 +0100

    Added bash script to run the image analogies code

---
+++ bash/run_lf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+date
+echo "****************************************"
+echo "****************************************"
+echo "Computation starting"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+EXE_PATH=~/workspaces/eclipse/lf/Release/lf
+echo $EXE_PATH
+cd ~/workspaces/matlab/vfx/Data/skinRender/microgeometry
+# Runs cores for each image
+EXT="c"
+#EXT=""
+I=1
+#while [  $I -lt 11 ]; do
+	echo "****************************************"
+	echo "Image num:" $I
+	echo "****************************************"
+	#echo $EXE_PATH "useYIQ: true" srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png"
+	$EXE_PATH srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png" useInterface: "false" useYIQ: "true"
+	let I=I+1 
+#done
+echo "Computation ended"
+echo "****************************************"
+echo "****************************************"
