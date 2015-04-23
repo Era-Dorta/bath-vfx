6f0dd1c9d8a73a000f3f6ca511d3ef1e00fd7f0e
Garoe <garoedp@gmail.com>
2015-04-23 09:02:24 +0100

    run lf, added if statement for grayscale images

--- bash/run_lf.sh
+++ bash/run_lf.sh
@@ -11,14 +11,19 @@ cd ~/workspaces/matlab/vfx/Data/skinRender/microgeometry
 EXT="c"
 #EXT=""
 I=1
-#while [  $I -lt 11 ]; do
+while [  $I -lt 11 ]; do
 	echo "****************************************"
 	echo "Image num:" $I
 	echo "****************************************"
 	#echo $EXE_PATH "useYIQ: true" srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png"
-	$EXE_PATH srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png" useInterface: "false" useYIQ: "true"
+	#$EXE_PATH srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png" useInterface: "false" &
+	if [ $EXT == "c" ]; then
+	   $EXE_PATH "useYIQ: true" srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png" useInterface: "false" 
+	else
+	  $EXE_PATH srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png" useInterface: "false" 
+	fi	
 	let I=I+1 
-#done
+done
 echo "Computation ended"
 echo "****************************************"
 echo "****************************************"
