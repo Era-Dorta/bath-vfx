708b1cc69af48ba647877e2447aa9a68faea5efc
Garoe <garoedp@gmail.com>
2015-04-23 09:05:17 +0100

    Readed indepent run to run lf

--- bash/run_lf.sh
+++ bash/run_lf.sh
@@ -18,9 +18,9 @@ while [  $I -lt 11 ]; do
 	#echo $EXE_PATH "useYIQ: true" srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png"
 	#$EXE_PATH srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png" useInterface: "false" &
 	if [ $EXT == "c" ]; then
-	   $EXE_PATH "useYIQ: true" srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png" useInterface: "false" 
+	   $EXE_PATH "useYIQ: true" srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png" useInterface: "false" &
 	else
-	  $EXE_PATH srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png" useInterface: "false" 
+	  $EXE_PATH srcExample: "original/A0_"$I$EXT".png" filteredExample: "original/A1_"$I$EXT".png" srcImage: "synthesized/B0_"$I$EXT".png" outputImageName: "synthesized/B1_"$I$EXT".png" useInterface: "false" &
 	fi	
 	let I=I+1 
 done
